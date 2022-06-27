6de2b52740caf343dba6fbb18cd9c87e7964bf8f
Pavelel Kalinin <mono-gluk@ya.ru>
2022-06-27 13:35:08 +0300

    First task list index error fixed

--- homework_1/task_1.py
+++ homework_1/task_1.py
@@ -1,8 +1,13 @@
+
 def domain_name(url):
     splitted_www = url.split('www.')
+    splitted_slash = url.split('//')
     if len(splitted_www) > 1:
         return splitted_www[1].split('.')[0]
-    return url.split('//')[1].split('.')[0]
+    elif len(splitted_slash) > 1:
+        return splitted_slash[1].split('.')[0]
+    else:
+        return url.split('.')[0]
 
 
 if __name__ == "__main__":
