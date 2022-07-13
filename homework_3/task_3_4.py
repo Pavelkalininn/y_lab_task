da6c52debbfd20a90468b7df8038d85e015aeee8
Павел Калинин <mono-gluk@ya.ru>
2022-07-13 10:29:22 +0300

    homework 3 complete with remarks second version

--- homework_3/task_3_4.py
+++ homework_3/task_3_4.py
@@ -11,45 +11,49 @@ def cash_decorator(func):
         cash_result = func(value)
         cash[value] = cash_result
         return cash_result
+
     return wrapper
 
 
-def repeat_decorator(func):
-    def wrapper(
-            call_count: int,
-            start_sleep_time: int,
-            factor: int,
-            border_sleep_time: int
-    ):
-        yield 'Начало работы'
-        time_delta = start_sleep_time
-        counter = 0
-        prev_check_time = 0
-        while counter < call_count:
-            current_time = int(time.perf_counter())
-            if counter:
-                time_delta *= 2 ** factor
-            if time_delta > border_sleep_time:
-                time_delta = border_sleep_time
-            if current_time == time_delta + prev_check_time:
-                counter += 1
-                func_result = func(2)
-                waiting_time = current_time - prev_check_time
-                yield (f'Запуск номер {counter}. '
-                       f'Ожидание: {waiting_time} секунд. '
-                       f'Результат декорируемой функций = {func_result}.')
-                prev_check_time = current_time
-        yield 'Конец работы'
-    return wrapper
+def repeat_decorator(call_count: int,
+                     start_sleep_time: int,
+                     factor: int,
+                     border_sleep_time: int):
+    def actual_decorator(func):
+        def wrapper(
+            *args, **kwargs
+        ):
+            yield 'Начало работы'
+            time_delta = start_sleep_time
+            counter = 0
+            prev_check_time = 0
+            while counter < call_count:
+                current_time = int(time.perf_counter())
+                if counter:
+                    time_delta *= 2 ** factor
+                if time_delta > border_sleep_time:
+                    time_delta = border_sleep_time
+                if current_time == time_delta + prev_check_time:
+                    counter += 1
+                    func_result = func(*args, **kwargs)
+                    waiting_time = current_time - prev_check_time
+                    yield (f'Запуск номер {counter}. '
+                           f'Ожидание: {waiting_time} секунд. '
+                           f'Результат декорируемой функций = {func_result}.')
+                    prev_check_time = current_time
+            yield 'Конец работы'
+
+        return wrapper
+    return actual_decorator
 
 
-@repeat_decorator
+@repeat_decorator(3, 2, 2, 7)
 @cash_decorator
 def multiplier(number: int):
     return number * 2
 
 
 if __name__ == '__main__':
-    result = multiplier(3, 3, 2, 15)
+    result = multiplier(3)
     for string in result:
         print(string)
